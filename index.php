acf33990f7faa81d2dad974d7faba581c8c5c9c2
118358721 <118358721@BISB40VL.bis.ad.ucc.ie>
2019-02-21 10:23:55 +0000

    third commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+
+<html>
+    
+    <head>
+        <meta charset="UTF-8"
+              <title></title>
+       
+    </head>
+    <body>
+        <?php
+            echo "hello world"
+        ?>
+        
+    </body>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+</html>
